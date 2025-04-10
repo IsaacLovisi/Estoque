70c2f91b4fc72eed63866c5c2151e79c5684bbf9
Isaac Lovisi <isaaclovisi@gmail.com>
2025-04-09 22:47:16 -0300

    Função excluir adicionada

--- main.py
+++ main.py
@@ -23,17 +23,8 @@ CREATE TABLE IF NOT EXISTS usuarios(
     )
 """)
 
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS logs(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        id_usuario INTEGER NOT NULL,
-        acao TEXT NOT NULL,
-        timestamp TEXT NOT NULL,
-        FOREIGN KEY (id_usuario) REFERENCES usuarios(id)
-        )
-    """)
 
-conn.commit()
+
 
 """def registrar_log(acao):
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -47,17 +38,17 @@ def criar_produto(nome, quantidade, preco):
         conn.commit()
         print("Produto adicionado com sucesso!")
         print("Aperte 1 para adicionar um novo produto/ Aperte qualquer número para volta ao menu ")
-        saida = int(input())
-        if saida != 1:
+        saida = input()
+        if saida != "1":
             break
-        if saida == 1:
+        if saida == "1":
             print("2")
             while True:
                 print("3")
                 nome = input("Digite o nome do produto (Aperte Enter para voltar ao menu inicial): ")
                 if nome == "":
                     print("4")
-                    break
+                    return
                 print("5")
                 cursor.execute("SELECT * FROM produtos WHERE nome = ?", (nome,))
                 search = cursor.fetchone()
@@ -69,18 +60,27 @@ def criar_produto(nome, quantidade, preco):
                         print("7")
                         quantidade = int(input(("Quantidade de itens: ")))
                         preco = float(input(("Valor unitário: ")))
-                        print("Produto adicionado com sucesso!")
-                        print("Aperte 1 para adicionar um novo produto/ Aperte qualquer número para volta ao menu ")
-                        saida = int(input())
-                        if saida == 1:
-                            print("8")
-                            continue
+                        if quantidade < 0 or preco <0:
+                            if quantidade <0:
+                                print(f"valor inválido ({quantidade})")
+                            else:
+                                print(f"valor inválido ({preco})")
                         else:
-                            print("9")
-                            return
+                            cursor.execute("INSERT INTO produtos(nome, quantidade, preco) VALUES (?, ?, ?)", (nome, quantidade, preco))
+                            conn.commit()
+                            print("Produto adicionado com sucesso!")
+                            print("Aperte 1 para adicionar um novo produto/ Aperte qualquer número para volta ao menu ")
+                            saida = input()
+                            if saida == "1":
+                                print("8")
+                                continue
+                            else:
+                                print("9")
+                                return
                     except ValueError:
-                        print("10")
-                        return
+                            print("10")
+                            return
+    conn.commit()
 
 def listar_produtos():
     cursor.execute("SELECT * FROM produtos")
@@ -95,7 +95,8 @@ def listar_produtos():
         print("Nenhum produto cadastrado")
 
 def atualizar_produto():
-    id_produto = int(input("Informe o ID do produto: "))
+    listar_produtos()
+    id_produto = int(input("\nInforme o ID do produto: "))
     cursor.execute("SELECT * FROM produtos WHERE id = ?", (id_produto,))
     produtos = cursor.fetchall()
     
@@ -109,11 +110,18 @@ def atualizar_produto():
 
     match opcao:
         case 1:
-                novo_valor = int(input("Nova quantidade: "))
+            novo_valor = int(input("Nova quantidade: "))
+            if novo_valor < 0:
+                print(f"fValor Indisponível ({novo_valor})")
+                return
+            else:
                 cursor.execute("UPDATE produtos SET quantidade = ? WHERE id = ?", (novo_valor, id_produto))
         case 2:
             novo_valor = float(input("Novo preço: "))
-            cursor.execute("UPDATE produtos SET preco = ? WHERE id = ?", (novo_valor, id_produto))
+            if novo_valor <= 0:
+                print(f"Valor Indisponível ({novo_valor})")
+            else:
+                cursor.execute("UPDATE produtos SET preco = ? WHERE id = ?", (novo_valor, id_produto))
         case _:
             print("Opção inválida.")
             return
@@ -123,75 +131,86 @@ def atualizar_produto():
     return 
 
 
-def excluir_produtos(nome, id_produto):
-    cursor.execute("DELETE FROM produtos WHERE nome = ? AND id = ?", (nome, id_produto))
-    conn.commit()
+def excluir_produtos(nome):
+    cursor.execute("SELECT * FROM produtos WHERE nome = ?", (nome,))
+    search = cursor.fetchone()
+    if search:
+        numb = input(f"Você tem certeza que deseja excluir o produto {search[1]}? Digite '1'\n:")
+        if numb == "1" and search:
+            cursor.execute("DELETE FROM produtos WHERE nome = ?", (nome,))
+            conn.commit()
+            print("Produto excluido com sucesso")
+    else:
+        print("Produto não existe")
 
+def menu_produtos():
+    while True:
+        print("-" * 42)
+        print("|         Selecione uma opção         |")
+        print("¯" * 42)
+        print("|1  -         Criar Produto           |")
+        print("|2  -         Listar Produto          |")
+        print("|3  -         Atualizar Produto       |")
+        print("|4  -         Remover Produto         |")
+        print("|5  -         Sair                    |")
+        print("-" * 42)
+        opcaoMenu = input(":")
+        match opcaoMenu:
+            case '1':
+                menu_criar_produto()
+            case '2':
+                listar_produtos()
+            case '3':
+                atualizar_produto()
+            case '4':
+                listar_produtos()
+                nome = input("\nDigite o nome do produto que deseja excluir\n:")
+                excluir_produtos(nome)
+            case '5':
+                return
+            case _:
+                print("Opção Inválida")
+
+def menu_criar_produto():
+            while True:
+                print("12")
+                nome = input(("Digite o nome do produto (Aperte Enter para voltar): "))
+                cursor.execute("SELECT * FROM produtos WHERE nome = ?", (nome,))
+                search = cursor.fetchone()
+                if nome == "":
+                    print("13")
+                    break
+                if search:
+                    print("14")
+                    print("Produto já existe")
+                else: 
+                    print("15")
+                    quantidade = int(input(("Quantidade de itens: ")))
+                    preco = float(input(("Valor unitário: ")))
+                    if quantidade < 0 or preco < 0:
+                        if quantidade < 0:
+                            print(f"Valor inválido ({quantidade})")
+                        else:
+                            print(f"Valor inválido ({preco})")
+                    else:
+                        criar_produto(nome, quantidade, preco)
+                        break
 
 def inicio():
     while True:
         print("-" * 42)
         print("|          BEM VINDO AO ESTOQUE          |")
         print("¯" * 42)
-        opcao = input("1 - Produtos\n2 - Logs\n3 - Área de Acesso\n4 - Sair do programa\n:")
+        opcao = input("1 - Produtos\n2 - Sair do programa\n:")
         if opcao == "":
             break
         match opcao:
             case '1':
-                while True:
-                    print("-" * 42)
-                    print("|         Selecione uma opção         |")
-                    print("¯" * 42)
-                    print("|1  -         Criar Produto           |")
-                    print("|2  -         Listar Produto          |")
-                    print("|3  -         Atualizar Produto       |")
-                    print("|4  -         Remover Produto         |")
-                    print("|5  -         Voltar ao menu          |")
-                    print("|6  -         Sair                    |")
-                    print("-" * 42)
-                    subopcao = input(":")
-                    match subopcao:
-                        case '1':
-                            while True:
-                                print("12")
-                                nome = input(("Digite o nome do produto (Aperte Enter para voltar): "))
-                                cursor.execute("SELECT * FROM produtos WHERE nome = ?", (nome,))
-                                search = cursor.fetchone()
-                                if nome == "":
-                                    print("13")
-                                    break
-                                if search:
-                                    print("14")
-                                    print("Produto já existe")
-                                else: 
-                                    print("15")
-                                    quantidade = int(input(("Quantidade de itens: ")))
-                                    preco = float(input(("Valor unitário: ")))
-                                    criar_produto(nome, quantidade, preco)
-                                    break
-                        case '2':
-                            listar_produtos()
-                        case '3':
-                            atualizar_produto()
-                        case '4':
-                            print("CRIAR FUNÇÃO DE REMOVER PRODUTO")#CRIAR FUNÇÃO DE REMOVER PRODUTO
-                        case '5':
-                            break
-                        case '6':
-                            return
-                        case _:
-                            print("Opção Inválida")
+                menu_produtos()
             case '2':
-                print()
-            case '3':
-                print()
-            case '4':
-                print("passou pelo break da area bem vindo")
                 break
             case _:
                 print("Valor inválido")
                 break  
 
 inicio()
-#excluir_produtos("Digite o nome do produto: ", 10)
-
